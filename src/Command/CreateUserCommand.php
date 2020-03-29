e2fdb85c1158fb81587053abf6bbbf4c9f242545
Your Name <you@example.com>
2020-03-29 12:08:19 +0300

    first

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,25 @@
+<?php
+// src/Command/CreateUserCommand.php
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:create-user';
+
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ...
+
+        return 0;
+    }
+}
